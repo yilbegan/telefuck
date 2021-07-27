7dc392565b1834de3bfb74d98d5ed77bb6794752
yilbegan <w2v@protonmail.com>
2021-07-27 22:24:45 +0300

    :bug: Remove config.DATABASE_URL

--- app/config.py
+++ app/config.py
@@ -2,7 +2,6 @@ from environs import Env
 
 env = Env()
 
-DATABASE_URL: str = env.str("DATABASE_URL")
 HEROKU_APP_NAME: str = env.str("HEROKU_APP_NAME")
 TELEGRAM_TOKEN: str = env.str("TELEGRAM_TOKEN")
 PORT: int = env.int("PORT")
